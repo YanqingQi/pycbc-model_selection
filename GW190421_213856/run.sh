0192d72585702e8db4d1595896e56c400b35675d
Yan-Qing Qi <58427930+YanqingQi@users.noreply.github.com>
2023-03-29 22:20:29 +0800

    Add files via upload

---
+++ GW190421_213856/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# configuration files
+CONFIG=inference-GW190421_213856.ini
+
+OUTPUT_PATH=GW190421_213856_inference.hdf
+
+# the following sets the number of cores to use; adjust as needed to
+# your computer's capabilities
+NPROCS=30
+
+# run sampler
+# Running with OMP_NUM_THREADS=1 stops lalsimulation
+# from spawning multiple jobs that would otherwise be used
+# by pycbc_inference and cause a reduced runtime.
+OMP_NUM_THREADS=1 \
+pycbc_inference --verbose \
+    --seed 1897234 \
+    --config-file ${CONFIG} \
+    --output-file ${OUTPUT_PATH} \
+    --nprocesses ${NPROCS} \
+    --force
